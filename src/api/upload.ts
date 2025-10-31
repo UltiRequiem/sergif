ecb26acbd22bea88376d3ba8bc78f302a8f44dc5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2025-10-31 12:53:38 -0500

    chore: get back to building

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -2,28 +2,28 @@ import { Buffer } from "node:buffer";
 import { TixteClient } from "@ultirequiem/tixte";
 import { createHandler } from "netfun";
 
-if (!process.env.TIXTE_API_KEY2) {
-  throw new Error("Missing TIXTE_API_KEY2 environment variable");
+if (!process.env.TIXTE_API_KEY) {
+	throw new Error("Missing TIXTE_API_KEY environment variable");
 }
 
-export const tixteClient = new TixteClient(process.env.TIXTE_API_KEY2, {
-  defaultURL: "ultirequiem.is-from.space",
+export const tixteClient = new TixteClient(process.env.TIXTE_API_KEY, {
+	defaultURL: "ultirequiem.is-from.space",
 });
 
 import type { Handler } from "netfun";
 
 export const upload: Handler = async (event) => {
-  if (!event.body) {
-    return { error: "Missing body", statusCode: 400 };
-  }
+	if (!event.body) {
+		return { error: "Missing body", statusCode: 400 };
+	}
 
-  const {
-    data: { url, direct_url },
-  } = await tixteClient.uploadFile(Buffer.from(event.body, "base64"), {
-    extension: "gif",
-  });
+	const {
+		data: { url, direct_url },
+	} = await tixteClient.uploadFile(Buffer.from(event.body, "base64"), {
+		extension: "gif",
+	});
 
-  return { url, direct_url };
+	return { url, direct_url };
 };
 
 export const handler = createHandler("POST", upload, { bodyRequired: true });
